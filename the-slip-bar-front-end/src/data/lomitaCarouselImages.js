bec9603f7d1bc489241fc236621d56f51430d1de
Shangita Paul <shangita.paul@gmail.com>
2024-04-05 13:06:07 -0700

    initial commit

---
+++ the-slip-bar-front-end/src/data/lomitaCarouselImages.js
@@ -0,0 +1,19 @@
+const lomitaCarouselImages = [
+  {
+    link: "/img/randy.png",
+    title: "Lomita",
+    description: "Neighborhood bar with a great atmosphere",
+  },
+  {
+    link: "/img/joints4jeremy.jpeg",
+    title: "Live Music",
+    description: "Check out our live music lineup in the 'Events' section",
+  },
+  {
+    link: "/img/lomita-patio.jpeg",
+    title: "Outdoor Patio",
+    description: "Enjoy the South Bay weather",
+  },
+];
+
+export default lomitaCarouselImages;
