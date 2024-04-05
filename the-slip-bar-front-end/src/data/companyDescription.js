9904ff5d72e89f304c75f91f2ed878c0bbee7fc4
Shangita Paul <shangita.paul@gmail.com>
2024-04-05 12:23:25 -0700

    initial commit

---
+++ the-slip-bar-front-end/src/data/companyDescription.js
@@ -0,0 +1,15 @@
+const COMPANY_DESCRIPTION = (
+    <div>
+        <p style={{ fontSize: '1em', fontFamily: 'Georgia' }}>
+            Whether in the waterfront in Redondo Beach, or in the cozy neighborhood nook of Lomita, we pride ourselves as a friendly local spot specializing in craft and locally brewed beers, spirits, and delicious eats!
+        </p>
+        <p style={{ fontSize: '1em', fontFamily: 'Georgia' }}>
+            At The Slip, we don't think the food at a bar has be flavorless. Along with delicious wings, pizza, burgers, and fries, we offer a Blue Zones Certified menu with plenty of healthy and vegetarian choices.
+        </p>
+        <p style={{ fontSize: '1em', fontFamily: 'Georgia' }}>
+            We also have gorgeous outdoor seating and live music and entertainment all week long. We are proud to be a part of the South Bay community and look forward to serving you soon!
+        </p>
+    </div>
+);
+
+export default COMPANY_DESCRIPTION;
