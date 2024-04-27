9a4faa258063c51559c486e770dc05eb295f6ffd
Shangita Paul <shangita.paul@gmail.com>
2024-04-27 15:01:14 -0700

    Update server.js to exclude favicon

--- server.js
+++ server.js
@@ -10,38 +10,12 @@ const PORT = process.env.PORT || 5000;
 // Serve static files from the 'frontend/dist' directory
 app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
-// Dummy job data with location information
-const jobs = [
-  {
-    id: '04/05',
-    time: '8:00 PM',
-    title: 'Michael Forbes Band',
-    description: 'Classic Rock',
-    location: 'Redondo Beach'
-  },
-  // Add other job objects here...
-];
-
-// Middleware
-app.use(express.json());
-
-// Routes
-app.get('/api/jobs', (req, res) => {
-  res.json(jobs);
-});
-
-app.get('/api/jobs/:id', (req, res) => {
-  const jobId = req.params.id;
-  const job = jobs.find(job => job.id === jobId);
-
-  if (!job) {
-    return res.status(404).json({ error: 'Job not found' });
-  }
-
-  res.json(job);
-});
-
 // Serve frontend
+app.get('/favicon.ico', (req, res) => {
+  // Send an empty response for the favicon request
+  res.status(204).end();
+});
+
 app.get('*', (req, res) => {
   // Render the Home component to HTML
   const html = ReactDOMServer.renderToString(React.createElement(Home));
