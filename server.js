996f58c35c76f2e06a404f888da01451064cef3d
Scott Sotka <ssotka@gmail.com>
2024-04-27 16:21:31 -0700

    ssotka - made try to get server to serve and index.html not to error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
-const Home = require('./frontend/dist/src/components/home.js'); // Correct file path for Home component
+//const Home = require('./frontend/dist/src/components/home.js'); // Correct file path for Home component
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,18 +10,25 @@ const PORT = process.env.PORT || 5000;
 // Serve static files from the 'frontend/dist' directory
 app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
+// set up a jobs endpoint to return a list of jobs
+app.get('/jobs', (req, res) => {
+  res.json([
+    { id: 1, title: 'Software Engineer', company: 'Google' },
+    { id: 2, title: 'Product Manager', company: 'Facebook' },
+    { id: 3, title: 'Data Scientist', company: 'Amazon' }
+  ]);
+});
+
 // Serve frontend
 app.get('/favicon.ico', (req, res) => {
   // Send an empty response for the favicon request
-  res.status(204).end();
+  res.sendFile(__dirname + '/frontend/dist/public/favicon.ico');
+
 });
 
 app.get('*', (req, res) => {
-  // Render the Home component to HTML
-  const html = ReactDOMServer.renderToString(React.createElement(Home));
-
-  // Serve the rendered HTML as the response
-  res.send(html);
+  // Redirect to the slipbar homepage
+  res.sendFile(__dirname + '/frontend/dist/public/index.html');
 });
 
 // Start server
